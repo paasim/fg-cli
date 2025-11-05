f6ce2d5cd69ce719c84d7460a34c107a93264ca8
Markus Paasiniemi <madp-git-7117@2pi.fi>
2025-11-05 08:22:08 +0200

    feat(api): use (requests) sessions to handle timeouts, which might also improve performance of paginated queries.

--- fg_cli/api.py
+++ fg_cli/api.py
@@ -1,32 +1,37 @@
 from time import sleep
 
-from requests import get
-from requests.exceptions import Timeout
+from requests import Session
+from requests.adapters import HTTPAdapter
 
 URL = "https://data.fingrid.fi/api"
 TIMEOUT = 20
 MAX_PAGE_SIZE = 20000
 
 
-def get_json(
-    url_path: str, params: dict, api_key: None | str, max_retries: int = 2
-) -> dict:
-    """get query with params to url_path, return resulting json as dict"""
+def _mk_session(api_key: None | str) -> Session:
     if api_key is None:
         raise ValueError("api_key missing")  # noqa: TRY003, EM101
-    headers = {"x-api-key": api_key}
+    s = Session()
+    s.mount(URL, HTTPAdapter(max_retries=3))
+    s.headers.update({"x-api-key": api_key})
+    return s
+
+
+def get_json(
+    url_path: str,
+    params: dict,
+    api_key_or_session: None | str | Session = None,
+) -> dict:
+    """get query with params to url_path, return resulting json as dict"""
     url = f"{URL}/{url_path}"
-    tries = 0
-    while True:
-        try:
-            resp = get(url, params=params, headers=headers, timeout=TIMEOUT)
-            resp.raise_for_status()
-            return resp.json()
-        except Timeout:
-            if tries >= max_retries:
-                raise
-            sleep(1)
-            tries += 1
+    if isinstance(api_key_or_session, Session):
+        resp = api_key_or_session.get(url, params=params, timeout=TIMEOUT)
+    else:
+        with _mk_session(api_key_or_session) as s:
+            resp = s.get(url, params=params, timeout=TIMEOUT)
+
+    resp.raise_for_status()
+    return resp.json()
 
 
 def get_paginated(
@@ -41,10 +46,13 @@ def get_paginated(
     data = resp["data"]
     next_page = resp["pagination"].get("nextPage")
 
-    while next_page is not None and (max_results is None or len(data) < max_results):
-        params["page"] = next_page
-        sleep(1)  # sleep between calls
-        resp = get_json(url, params, api_key)
-        data += resp["data"]
-        next_page = resp["pagination"].get("nextPage")
-    return data
+    with _mk_session(api_key) as session:
+        while next_page is not None and (
+            max_results is None or len(data) < max_results
+        ):
+            params["page"] = next_page
+            sleep(1)  # sleep between calls
+            resp = get_json(url, params, session)
+            data += resp["data"]
+            next_page = resp["pagination"].get("nextPage")
+        return data
